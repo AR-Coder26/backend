4bbac634724d0475f6444fe0c2d4fbfe2ed0d1c5
Ammad <ar.dev.tech47@gmail.com>
2026-09-02 13:58:51 +0500

    updating (createAdmin.js, seed.js, server.js) to deploy beckend on serverless envirenment

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-const dns = require('node:dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+if (process.env.NODE_ENV === 'development') {
+  const dns = require('node:dns');
+  dns.setServers(['8.8.8.8', '1.1.1.1']);
+}
 
 const dotenv = require('dotenv');
 dotenv.config();
